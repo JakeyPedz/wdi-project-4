9f70ad35781ba97d6eab4cb5508e22c795b50c24
JakeyPedz <j.willisfleming@gmail.com>
2015-12-15 11:47:16 +0000

    set up app js in API

--- api/app.js
+++ api/app.js
@@ -16,3 +16,53 @@ var config = require('./config/config');
 var routes = require('./config/routes');
 var secret = require('./config/config').secret; // Set up secret used by JWT
 require('./config/passport')(passport);
+
+// Hook into mongoDB via mongoose
+mongoose.connect(config.database);
+
+// Create App
+var app = express();
+
+// Set our app to use packages
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use(cors());
+app.use(passport.initialize());
+
+
+// Set-up method-override
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method
+    delete req.body._method
+    return method
+  };
+}));
+
+// Set app to use JWTs when called though '/api'
+app.use('/api', expressJWT({ secret: secret })
+  .unless({
+    path: [
+      { url: '/api/register', methods: ['POST'] },
+      { url: '/api/login', methods: ['POST'] },
+      // { url: '/api/auth/facebook', methods: ['POST'] },
+      // { url: '/api/auth/facebook/callback', methods: ['GET'] },
+      { url: '/api/projects', methods: ['GET'] }
+    ]
+  }));
+
+// Display user friendly error when 401 occurs
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    return res.status(401).json({message: 'Unauthorized request.'});
+  };
+  next();
+});
+
+// Tell app to use routing via the '/api' sub-domain
+app.use("/api", routes);
+
+app.listen(3000);
+console.log("Express is alive and listening on port 3000.");
