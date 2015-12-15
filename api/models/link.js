218bb125f5c96e14553d32790b5ffe9e276166f8
JakeyPedz <j.willisfleming@gmail.com>
2015-12-15 09:27:50 +0000

    still need to do some hrefs and some titles for certain platforms

--- api/models/link.js
+++ api/models/link.js
@@ -1,12 +1,13 @@
 var mongoose     = require("mongoose");
 // var findOrCreate = require("mongoose-findorcreate");
 
-var linkSchema   = mongoose.Schema({
+var projectSchema   = mongoose.Schema({
   title: String,
   description: String,
-  logo: String 
+  logo: String
+  href: { type: String, required: true, unique: true }
 });
 
 // linkSchema.plugin(findOrCreate);
 
-module.exports = mongoose.model("link", linkSchema);
+module.exports = mongoose.model("project", projectSchema);
