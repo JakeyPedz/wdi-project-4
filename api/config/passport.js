d27a36d2785e47bf0c5567a4147cb53c649ead12
JakeyPedz <j.willisfleming@gmail.com>
2015-12-15 12:02:59 +0000

    setting up facebook authentication routes and config

--- api/config/passport.js
+++ api/config/passport.js
@@ -1,5 +1,6 @@
 var LocalStrategy = require('passport-local').Strategy;
-// var GitHubStrategy = require('passport-github2').Strategy;
+var passport = require('passport')
+  , FacebookStrategy = require('passport-facebook').Strategy;
 var User = require('../models/user');
 
 // var GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
@@ -32,15 +33,17 @@ module.exports = function (passport) {
     });
   }));
 
-//   passport.use(new GitHubStrategy({
-//     clientID: GITHUB_CLIENT_ID,
-//     clientSecret: GITHUB_CLIENT_SECRET,
-//     callbackURL: "http://localhost:3000/auth/github/callback"
-//   },
-//   function(accessToken, refreshToken, profile, done) {
-//     User.findOrCreate({ githubId: profile.id }, function (err, user) {
-//       return done(err, user);
-//     });
-//   }
-// ))
+  passport.use(new FacebookStrategy({
+      clientID: FACEBOOK_APP_ID,
+      clientSecret: FACEBOOK_APP_SECRET,
+      callbackURL: "http://localhost:3000/auth/facebook/callback" // NEED TO PUT THIS IN ROUTES
+    },
+    function(accessToken, refreshToken, profile, done) {
+      User.findOrCreate(..., function(err, user) {
+        if (err) { return done(err); }
+        done(null, user);
+      });
+    }
+  ));
+
 };
--- api/config/routes.js
+++ api/config/routes.js
@@ -16,3 +16,13 @@ router.route('/projects/:title')
 .get(projectsController.getProject)
 
 module.exports = router 
+
+router.post('/auth/github', passport.authenticate('github', { scope: [ 'user:email' ] }), authController.gitRegister);
+
+router.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), authController.gitCallback);
+
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/',
+                                      failureRedirect: '/login' }));
