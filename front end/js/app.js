b7012699bf4d9c1a67055e3fdb32c6010b101498
JakeyPedz <j.willisfleming@gmail.com>
2015-12-15 17:48:09 +0000

    added a main router to the app js based on lesson notes

--- front end/js/app.js
+++ front end/js/app.js
@@ -1,3 +1,17 @@
 angular
-  .module('Qrowded', ['ui.router'])
-  .config(MainRouter);
+.module('Qrowded', ['ui.router'])
+.config(MainRouter);
+
+
+function MainRouter($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "home.html",
+    })
+    .state('archive', {
+      url: "/archive",
+      templateUrl: "archive.html",
+    });
+
+}
