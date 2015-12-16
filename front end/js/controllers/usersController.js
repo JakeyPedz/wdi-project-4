8084692a189e84d7171af908de6943d79c919a7f
JakeyPedz <j.willisfleming@gmail.com>
2015-12-16 11:31:24 +0000

    had a crack at index html with ng show and hide for log in as well as form

---
+++ front end/js/controllers/usersController.js
@@ -0,0 +1,53 @@
+angular
+  .module('Qrowded')
+  .controller('usersController', UserController)
+
+UserController.$inject = ['User', 'TokenService']
+function UserController(User, TokenService) {
+  var self = this;
+
+  self.all    = [];
+  self.user   = {};
+
+  function handleLogin(res) {
+    var token = res.token ? res.token : null;
+    
+    // Console.log our response from the API
+    if (token) {
+      console.log(res);
+      self.getUsers();
+      self.user = TokenService.decodeToken();
+    }
+
+    self.message = res.message;
+  }
+
+  self.login = function() {
+    User.login(self.user, handleLogin);
+  }
+
+  self.register = function() {
+    User.register(self.user, handleLogin);
+  }
+
+  self.disappear = function() {
+    TokenService.removeToken();
+    self.all   = [];
+    self.user = {}; 
+  }
+
+  self.getUsers = function() {
+    self.all = User.query();
+  }
+
+  self.isLoggedIn = function(){
+    return !!TokenService.getToken();
+  }
+
+  if (self.isLoggedIn()) {
+    self.getUsers();
+    self.user = TokenService.decodeToken();
+  }
+
+  return self;
+}
