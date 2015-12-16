33eb515e58ecd65fc6c19b2f758597d00ba095fe
JakeyPedz <j.willisfleming@gmail.com>
2015-12-16 16:37:16 +0000

    set up page partials

--- front end/js/app.js
+++ front end/js/app.js
@@ -1,20 +1,31 @@
 angular
-  .module('Qrowded', ['angular-jwt', 'ui.router', 'ngResource'])
-  .config(MainRouter)
-  .constant("API", "http://localhost:3000/api")
-  .config(function($httpProvider){
-    $httpProvider.interceptors.push('authInterceptor')
-  });
+.module('Qrowded', ['angular-jwt', 'ui.router', 'ngResource'])
+.config(MainRouter)
+.constant("API", "http://localhost:3000/api")
+.config(function($httpProvider){
+  $httpProvider.interceptors.push('authInterceptor')
+});
 
 function MainRouter($stateProvider, $urlRouterProvider) {
   $stateProvider
-    .state('home', {
-      url: "/",
-      templateUrl: "home.html",
-    })
-    .state('archive', {
-      url: "/archive",
-      templateUrl: "archive.html",
-    });
-
+  .state('home', {
+    url: "/",
+    templateUrl: "partials/home.html",
+  })
+  .state('viewProjects', {
+    url: "/projects",
+    templateUrl: "projects.html",
+    controller: 'projectsController'
+  })
+  .state('login', {
+    url: "/login",
+    templateUrl: "partials/login.html"
+  })
+  .state('register', {
+    url: "/register",
+    templateUrl: "partials/register.html",
+  })
+  
+  $urlRouterProvider.otherwise("/");
+  
 }
