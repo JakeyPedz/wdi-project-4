66f663924437ce60f9d50b6509f9ae6ae88b76dc
JakeyPedz <j.willisfleming@gmail.com>
2015-12-16 10:58:02 +0000

    added user js model in the front end

---
+++ front end/js/models/user.js
@@ -0,0 +1,26 @@
+angular
+  .module('Qrowded')
+  .factory('Agent', Agent);
+
+USER.$inject = ['$resource', 'API'];
+function User($resource, API) {
+
+  return $resource(
+    API+'/users/:id',
+    {id: '@id'},
+    { 'get':       { method: 'GET' },
+      'save':      { method: 'POST' },
+      'query':     { method: 'GET', isArray: true},
+      'remove':    { method: 'DELETE' },
+      'delete':    { method: 'DELETE' },
+      'login': {
+        url: API + '/login',
+        method: "POST"
+      },
+      'register': {
+        url: API + '/register',
+        method: "POST"
+      }
+    }
+  );
+}
